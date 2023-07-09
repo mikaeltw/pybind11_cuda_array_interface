f8df0a89453e98355b53f75ce5f748d57a623edc
Mikael Twengström <m.twengstrom@gmail.com>
2023-07-09 15:16:34 +0200

    CMake issue for cuda linking, but committing prep of build with scikit-build regardless

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import os
-
+import toml
+import re
+import sys
 
 
 from skbuild import setup
 from setuptools import find_packages
+from setuptools_scm import get_version
 
 
-with open("README.md", "r") as f:
-    long_description = f.read()
+def minimum_requirements_of_non_python_dependencies():
+    return ['cuda>=10.1',
+            'cxx==14',]
 
 
 def get_current_directory(abs_path=False):
@@ -15,6 +19,7 @@ def get_current_directory(abs_path=False):
         return os.path.dirname(os.path.realpath(__file__))
     else:
         return "."
+
 
 def get_cmake_build_dir(abs_path=False, cmake_build_dir="build"):
     if abs_path:
@@ -25,43 +30,74 @@ def get_cmake_build_dir(abs_path=False, cmake_build_dir="build"):
         return cmake_build_dir
 
 
+def get_version_of_package_as_dependency_str():
+    version = get_version()
+    return [''.join(('package==', version))]
+
+
+def get_dependencies_as_cmake_args():
+    # Load the pyproject.toml file
+    pyproject = toml.load('pyproject.toml')
+
+    # Get the build system requirements
+    build_requires = pyproject['build-system']['requires']
+    python_requires = [''.join(('python', pyproject['project']['requires-python']))]
+    non_python_requires = minimum_requirements_of_non_python_dependencies()
+    version_requires = get_version_of_package_as_dependency_str()
+
+    package_list = build_requires + python_requires + non_python_requires + version_requires
+
+    separators = ['>=', '==']
+    separator_pattern = '|'.join(re.escape(separator) for separator in separators)
+    versions = {key: value for key, value in (re.split(separator_pattern, s) for s in package_list if re.search(separator_pattern, s))}
+
+    cmake_dependency_args = []
+    for pkg, version in versions.items():
+        cmake_dependency_args.append(f'-D{pkg.upper()}_MVERSION={version}')
+
+    return cmake_dependency_args
+
+
+def get_testbuild_args_as_cmake_args():
+    return ['-DDOWNLOAD_GTEST=ON',
+            '-DBUILD_GTESTS=ON',
+            '-DBUILD_PYTESTS=ON']
+
+
+def get_linux_cmake_args():
+    return ['-DCMAKE_INSTALL_LIBDIR=lib',]
+
+
+def get_windows_cmake_args():
+    ['-GNinja',
+     '-Dgtest_force_shared_crt=ON',]
+
+
+def get_macosx_cmake_args():
+    return ['-DCMAKE_INSTALL_LIBDIR=lib',]
+
+
 def get_cmake_args(dev=True):
+    cmake_args = []
     if dev:
-        return ['-DDOWNLOAD_GTEST=ON',
-                '-DBUILD_GTESTS=ON',
-                '-DBUILD_PYTESTS=ON']
+        cmake_args += get_testbuild_args_as_cmake_args()
+    LINUX = sys.platform.startswith("linux")
+    MACOS = sys.platform.startswith("darwin")
+    WIN = sys.platform.startswith("win32") or sys.platform.startswith("cygwin")
+    if LINUX:
+        cmake_args += get_linux_cmake_args()
+    elif MACOS:
+        cmake_args += get_macosx_cmake_args()
+    elif WIN:
+        cmake_args += get_windows_cmake_args()
     else:
-        return ['']
+        assert("Unknown platform.")
+    return cmake_args
 
-
-
-# if not os.path.exists(get_cmake_build_dir()):
-#     os.makedirs(get_cmake_build_dir())
 
 setup(
-    name='pybind11_cuda_array_interface',
-    version='0.0.1',
     packages=find_packages(),
-    author="Mikael Twengström",
-    author_email="m.twengstrom@gmail.com",
-    description="pybind11 headers supporting the __cuda_array_interface__",
-    long_description=long_description,
-    license="BSD 3-Clause",
     platforms=["Linux", "Windows", "MacOSX"],
-    long_description_content_type='text/markdown',
-    url="https://github.com/mikaeltw/pybind11_cuda_array_interface",
-    classifiers=[
-        "Programming Language :: C++",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: BSD 3-Clause",
-    ],
-    install_requires=[''],
-    python_requires=">=3.7",
     #cmake_install_dir=get_cmake_build_dir(),
     #cmake_source_dir=get_current_directory(),
     cmake_args=get_cmake_args()
