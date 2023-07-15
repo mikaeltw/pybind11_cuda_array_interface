2c3866b75062ab956c4b932985228f109425bea7
Mikael Twengström <m.twengstrom@gmail.com>
2023-07-16 00:03:24 +0200

    Add install script for cupy and a first shot at a gh yaml file

---
+++ tests/install_cupy.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+import re
+
+def get_cuda_version():
+    version = subprocess.run(["nvcc", "--version"], capture_output=True, text=True)
+    version = version.stdout.split()[-1]  # get the version string
+    version = re.split('_|\.', version)[1:3] # get major and minor version
+    return "".join(version)
+
+def install_cupy():
+    cuda_version = get_cuda_version()
+    cupy_package = "".join(("cupy-cuda", cuda_version))
+    subprocess.run([sys.executable, "-m", "pip", "install", cupy_package])
+
+if __name__ == "__main__":
+    install_cupy()
