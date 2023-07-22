0c4b54d1efee8fafc3b7f0b548e7045dcff7bc68
Mikael Twengström <m.twengstrom@gmail.com>
2023-07-22 21:52:20 +0200

    Adjust test to the new return type from cast and add a passthrough test to pytest

--- tests/pytest/pybind11_cuda_array_interface_test.py
+++ tests/pytest/pybind11_cuda_array_interface_test.py
@@ -17,3 +17,13 @@ def test_saxpy_kernel():
 
     cp.testing.assert_allclose(s1, s2)
     cp.testing.assert_allclose(s1, s3)
+
+
+def test_sendreceive():
+    cupy_test_array = cp.array([41.0, 62.0, 98.0], dtype=cp.float32)
+    numpy_test_array = cp.asnumpy(cupy_test_array)
+    returned_cupy_array = pycai.receive_and_return_cuda_array_interface(cupy_test_array)
+
+    assert hasattr(returned_cupy_array, "__cuda_array_interface__")
+
+    np.testing.assert_allclose(numpy_test_array, cp.asnumpy(returned_cupy_array))
