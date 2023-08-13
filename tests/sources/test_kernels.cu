32da1226d62e67294402f72eeb8e47f62b816296
Mikael Twengström <m.twengstrom@gmail.com>
2023-08-13 23:33:18 +0200

    enforced clang-format on codebase

--- tests/sources/test_kernels.cu
+++ tests/sources/test_kernels.cu
@@ -5,13 +5,14 @@
 ## BSD-3 Clause License. For full terms see the included LICENSE file.
 */
 
-__global__
-void saxpy_kernel(float *s, float *x, float *y, int a, int n) {
+__global__ void saxpy_kernel(float *s, float *x, float *y, int a, int n)
+{
     for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x) {
-         s[i] = a * x[i] + y[i];
+        s[i] = a * x[i] + y[i];
     }
 }
 
-void call_saxpy(float *s, float *x, float *y, int a, int n) {
+void call_saxpy(float *s, float *x, float *y, int a, int n)
+{
     saxpy_kernel<<<256, 32>>>(s, x, y, a, n);
 }
--- tests/sources/test_kernels.hpp
+++ tests/sources/test_kernels.hpp
@@ -9,7 +9,6 @@
 
 #include <cuda_runtime.h>
 
-__global__
-void saxpy_kernel(float *s, float *x, float *y, int a, int n);
+__global__ void saxpy_kernel(float *s, float *x, float *y, int a, int n);
 
 void call_saxpy(float *s, float *x, float *y, int a, int n);
